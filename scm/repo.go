51e3c731fa6e8cd9b2ef4d380d9a0fc5de826aeb
Marc-Antoine Ruel <maruel@chromium.org>
2015-05-16 22:08:25 -0400

    More native git handling code.
    This will permit native handling of stash/restore.

--- scm/repo.go
+++ scm/repo.go
@@ -5,15 +5,31 @@
 package scm
 
 import (
+	"errors"
 	"fmt"
 	"path/filepath"
+	"strings"
 
 	"github.com/maruel/pre-commit-go/internal"
 )
 
+// Repo represents a source control management checkout.
 type Repo interface {
+	// Root returns the root directory of this repository.
 	Root() string
+	// PreCommitHookPath returns the path to the bash script called as part of a
+	// commit.
 	PreCommitHookPath() (string, error)
+	// HEAD returns the HEAD commit hash.
+	HEAD() string
+	// Untracked returns the list of untracked files.
+	Untracked() ([]string, error)
+	// Unstaged returns the list with changes not in the staging index.
+	Unstaged() ([]string, error)
+	// Stash stashes the content that is not in the index.
+	Stash() (bool, error)
+	// Stash restores the stash generated from Stash.
+	Restore() error
 }
 
 // GetRepo returns a valid Repo if one is found.
@@ -45,6 +61,65 @@ func (g *git) PreCommitHookPath() (string, error) {
 		}
 	}
 	return filepath.Join(g.gitDir, "hooks", "pre-commit"), nil
+}
+
+func (g *git) HEAD() string {
+	if _, _, err := g.capture("rev-parse", "--verify", "HEAD"); err == nil {
+		return "HEAD"
+	}
+	// Initial commit: diff against an empty tree object
+	return "4b825dc642cb6eb9a060e54bf8d69288fbee4904"
+}
+
+func (g *git) Untracked() ([]string, error) {
+	out, _, err := g.capture("ls-files", "--others", "--exclude-standard")
+	return strings.Split(out, "\n"), err
+}
+
+func (g *git) Unstaged() ([]string, error) {
+	out, _, err := g.capture("diff", "--name-only", "--no-color", "--no-ext-diff")
+	return strings.Split(out, "\n"), err
+}
+
+func (g *git) Stash() (bool, error) {
+	if untracked, err := g.Untracked(); err != nil {
+		return false, err
+	} else if len(untracked) != 0 {
+		return false, errors.New("can't stash if there are untracked files")
+	}
+	if unstaged, err := g.Unstaged(); err != nil {
+		return false, err
+	} else if len(unstaged) == 0 {
+		// No need to stash, there's no unstaged files.
+		return false, nil
+	}
+
+	oldStash, _, _ := g.capture("rev-parse", "-q", "--verify", "refs/stash")
+	if out, e, err := g.capture("stash", "save", "-q", "--keep-index"); e != 0 || err != nil {
+		return false, fmt.Errorf("failed to stash: %s\n%s", err, out)
+	}
+	newStash, e, err := g.capture("rev-parse", "-q", "--verify", "refs/stash")
+	if e != 0 || err != nil {
+		return false, fmt.Errorf("failed to parse stash: %s\n%s", err, newStash)
+	}
+	return oldStash != newStash, err
+}
+
+func (g *git) Restore() error {
+	if out, e, err := g.capture("reset", "--hard", "-q"); e != 0 || err != nil {
+		return fmt.Errorf("git reset failed: %s\n%s", err, out)
+	}
+	if out, e, err := g.capture("stash", "apply", "--index", "-q"); e != 0 || err != nil {
+		return fmt.Errorf("stash reapplication failed: %s\n%s", err, out)
+	}
+	if out, e, err := g.capture("stash", "drop", "-q"); e != 0 || err != nil {
+		return fmt.Errorf("dropping temporary stash failed: %s\n%s", err, out)
+	}
+	return nil
+}
+
+func (g *git) capture(args ...string) (string, int, error) {
+	return internal.CaptureWd(g.root, append([]string{"git"}, args...)...)
 }
 
 // getGitDir returns the .git directory path.
