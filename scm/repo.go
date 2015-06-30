maruel/pre-commit-go

commit 7b545701fdd0b12d1b6763b4e3d995edf237d422

Show list of untracked files.

scm/repo.go
@@ -305,7 +305,7 @@ func (g *git) Stash() (bool, error) {
 		if untracked := g.untracked(); untracked == nil {
 			errUntrackedCh <- errors.New("failed to get list of untracked files")
 		} else if len(untracked) != 0 {
-			errUntrackedCh <- errors.New("can't stash if there are untracked files")
+			errUntrackedCh <- errors.New(fmt.Sprintf("can't stash if there are untracked files: %q", untracked))
 		} else {
 			errUntrackedCh <- nil
 		}

commit 4f990852821a33a5a1856ac4db4023bbb8f369d7

Fix lint regression in 7b545701fdd0b12d1b6763b4e3d995edf237d422.

scm/repo.go
@@ -305,7 +305,7 @@ func (g *git) Stash() (bool, error) {
 		if untracked := g.untracked(); untracked == nil {
 			errUntrackedCh <- errors.New("failed to get list of untracked files")
 		} else if len(untracked) != 0 {
-			errUntrackedCh <- errors.New(fmt.Sprintf("can't stash if there are untracked files: %q", untracked))
+			errUntrackedCh <- fmt.Errorf("can't stash if there are untracked files: %q", untracked)
 		} else {
 			errUntrackedCh <- nil
 		}